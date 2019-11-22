670498ad8e706889bc7c667fd8aec13dd6a4b6e3
Lovis Anderson <anderson@zib.de>
2019-11-22 10:57:33 +0100

    add: (WIP) image transformation/parsing and torchvision tools

--- data.py
+++ data.py
@@ -1,7 +1,8 @@
 import os
 import numpy as np
-import torch
 from PIL import Image
+import torch
+from scipy import ndimage
 
 
 class RooftopDataset(object):
@@ -26,21 +27,84 @@ class RooftopDataset(object):
                 img = img.convert("RGB")
         with Image.open(mask_path) as mask:
             mask = mask.convert('L')
+        mask_array = np.array(mask)
+        # make the picture binary. 100 is arbitrary but seems to work for dataset
+        mask_array[np.where(mask_array > 100)] = 255
+        mask_array[np.where(mask_array <= 100)] = 0
+
+        boxes = []
+        # find different objects (roofs) in mask
+        label_img, nr_labels = ndimage.label(mask_array)
+        for i in range(1, nr_labels + 1):
+            pos = np.where(label_img == i)
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+        labels = torch.ones((nr_labels,), dtype=torch.int64)
+
+        # instances are encoded as different colors
+        obj_ids = np.unique(label_img)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+        masks = label_img == obj_ids[:, None, None]
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((nr_labels,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        # suppose all instances are not crowd (artifact from coco pretrained net)
+        iscrowd = torch.zeros((nr_labels,), dtype=torch.int64)
+
+        # suppose all instances are not crowd
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
         if self.transforms is not None:
-            img, mask = self.transforms(img, mask)
+            img, target = self.transforms(img, target)
 
-        # convert the PIL Images into  numpy arrays
+        return img, target
 
-        img = np.array(img)
-        mask = np.array(mask)
-        mask = mask.reshape((1, 256, 256))
-        if self.grayscale:
-            img = img.reshape(1, 256, 256)
+    def save_color_encoded_images(self, subfolder='color_encoded'):
+        color_codes = [10, 40, 70, 100, 130, 160, 190, 220, 250]
 
-        # Non-zero means that this pixel belongs to a rooftop
-        mask[np.where(mask != 0)] = 1
+        subfolder_path = os.path.join(self.root, subfolder)
+        if not os.path.exists(subfolder_path):
+            os.mkdir(subfolder_path)
+        for mask_file_name in self.masks:
+            print(f'filename: {mask_file_name}')
+            mask_path = os.path.join(self.root, "labels", mask_file_name)
+            with Image.open(mask_path) as mask:
+                mask = mask.convert('L')
+            mask_array = np.array(mask)
+            # make the picture binary. 100 is arbitrary but seems to work for dataset
+            mask_array[np.where(mask_array > 100)] = 255
+            mask_array[np.where(mask_array <= 100)] = 0
+            label_img, nr_labels = ndimage.label(mask_array)
 
-        return torch.from_numpy(img), torch.from_numpy(mask)
+            colored = mask_array
+            print(f'nr labels: {nr_labels}')
+            for i in range(1, nr_labels + 1):
+                colored[np.where(label_img == i)] = color_codes[i-1]
+            img = Image.fromarray(colored, 'L')
+            img.save(os.path.join(subfolder_path, mask_file_name))
 
     def __len__(self):
         return len(self.imgs)
+
+
+# data_root_path = 'images'
+# dataset = RooftopDataset(root=data_root_path, grayscale=True, transforms=None)
+# img, mask = dataset[0]
